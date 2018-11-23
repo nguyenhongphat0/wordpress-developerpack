44a5285caba3d1d6371e8e149f1384fa2fe17d1c
nguyenhongphat0 <nguyenhongphat28121998@gmail.com>
2018-11-23 09:28:46 +0700

    init project

---
+++ developerpack.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Plugin Name:  Developer Pack
+ * Description:  This module contain everything a wordpress developer need.
+ * Author:       nguyenhongphat0
+ * Author URI:   https://nguyenhongphat0.github.io
+ * License:      GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-3.0.en.html
+ * Text Domain:  developerpack
+ */
+
+echo 'Hello';
